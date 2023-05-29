a45443925f6981e85261db73218a3d8a746d56a6
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-05-29 22:16:34 +0530

    Create ArithmeticOperations.c

---
+++ ArithmeticOperations.c
@@ -0,0 +1,21 @@
+                          //         Write a c program to implement all the arithmetic operators
+
+#include<stdio.h>
+                                 
+
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers : ");
+    scanf("%d %d",&a,&b);
+
+printf("Addition : %d\n",a+b);
+printf("Subtraction : %d\n",a-b);
+printf("Multiplication : %d\n",a*b);
+printf("Division : %d\n",a/b);
+printf("modulus : %d\n",a%b);
+
+return 0;
+
+}
