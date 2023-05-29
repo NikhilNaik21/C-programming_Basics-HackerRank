44973aa5b182b3141d3a4061b5aa9734f35a64a3
NikhilNaik <111115551+NikhilNaik21@users.noreply.github.com>
2023-05-29 22:18:52 +0530

    Create Asciivalue.c

---
+++ Asciivalue.c
@@ -0,0 +1,17 @@
+
+//Write a c program to ASCII value of any character.
+
+#include<stdio.h>
+                                 
+
+
+int main()
+{
+    char ch;
+    printf("Enter Alphabet : ");
+    scanf("%c",&ch);
+
+    printf("Ascii Value = %d",ch);
+
+    return 0;
+}
